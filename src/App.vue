f7b18630e09c48ef0f29314f8cf4a3d49fcbfc41
namecoco256 <harinezumi-o_n-o_k-2016@outlook.jp>
2024-11-09 19:54:30 +0900

    I make palette

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,63 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue'
+const canvasSize = { width: 640, height: 400 }
+
+let canvas;
+let ctx;
+const palette = [
+  [0, 0, 0],
+  [0, 0, 245],
+  [234, 51, 35],
+  [234, 51, 247],
+  [117, 251, 76],
+  [117, 251, 253],
+  [255, 255, 84],
+  [255, 255, 255],
+  [119, 119, 119],
+  [0, 0, 163],
+  [156, 31, 20],
+  [156, 31, 164],
+  [76, 167, 48],
+  [76, 167, 169],
+  [170, 170, 53],
+  [170, 170, 170],
+  [89, 128, 163],
+  [75, 61, 60],
+  [88, 98, 201],
+  [106, 162, 247],
+  [113, 164, 86],
+  [144, 208, 206],
+  [138, 131, 129],
+  [133, 79, 81],
+  [148, 129, 241],
+  [154, 114, 140],
+  [168, 149, 127],
+  [197, 192, 192],
+  [207, 217, 89],
+  [207, 192, 164],
+  [225, 150, 68],
+  [224, 119, 179],
+  [227, 96, 74],
+  [235, 161, 158],
+  [246, 233, 218],
+  [243, 207, 102],
+  [248, 233, 87]
+]
+onMounted(()=>{
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "rgb(11,45,14)";
+  ctx.fillRect(0,0,canvasSize.width,canvasSize.height)
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <canvas id="canvas" :width="canvasSize.width" :height="canvasSize.height"></canvas>
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
