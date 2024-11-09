b76e9d69b2f1c1a3cf902024702d931db3825696
ray-chan-dayo <64572413+ray-chan-dayo@users.noreply.github.com>
2024-11-09 20:10:40 +0900

    errorhandle

--- src/libs/colorPalette.js
+++ src/libs/colorPalette.js
@@ -1,15 +1,15 @@
 // import { colorPalette } from "./libs/colorPalette.js"
 export function colorPalette(originalColor, palette) {
     // originalColor = [255,255,255]; //[r,g,b]
-    if (!isColor(originalColor)) console.error("Invalid arguments passed to function colorPalette(): originalColor is not a color.");
+    if (!isColor(originalColor)) console.error(`Invalid arguments passed to function colorPalette(): originalColor is not a color. (${originalColor})`);
     // エラー処理
-    if (typeof palette != "object") console.error("Invalid arguments passed to function colorPalette(): palette is not an object");
+    if (typeof palette != "object") console.error(`Invalid arguments passed to function colorPalette(): palette is not an object. (${palette})`);
     let leastDiff = 255 * 255 * 3;
     for (let i = 0; i < palette.length; i++) {
         
         let result;
         const paletteColor = palette[i];
-        if (!isColor(paletteColor)) console.error(`Invalid arguments passed to function colorPalette(): palette[${i}] is not a color.`);
+        if (!isColor(paletteColor)) console.error(`Invalid arguments passed to function colorPalette(): palette[${i}] is not a color. (${paletteColor})`);
         const diff = 
         ( originalColor[0] - paletteColor[0] ) ^ 2 +
         ( originalColor[1] - paletteColor[1] ) ^ 2 +
@@ -19,6 +19,7 @@ export function colorPalette(originalColor, palette) {
             leastDiff = diff;
         }
     }
+    if (typeof palette != "number") console.error(`Unexpected error at function colorPalette(): return value is not a number. (${i})`);
     return i
 }
 
