9d102b643e0947c2f5e8944d13d782decc7c0f38
ray-chan-dayo <64572413+ray-chan-dayo@users.noreply.github.com>
2024-11-10 02:06:25 +0900

    fix2

--- src/libs/colorPalette.js
+++ src/libs/colorPalette.js
@@ -2,14 +2,14 @@
 export function colorPalette(originalColor, palette) {
     let result;
     // originalColor = [255,255,255]; //[r,g,b]
-    // if (!isColor(originalColor)) console.error(`Invalid arguments passed to function colorPalette(): originalColor is not a color. (${originalColor})`);
     // エラー処理
-    // if (typeof palette != "object") console.error(`Invalid arguments passed to function colorPalette(): palette is not an object. (${palette})`);
+    if (!isColor(originalColor)) console.error(`Invalid arguments passed to function colorPalette(): originalColor is not a color. (${originalColor})`);
+    if (typeof palette != "object") console.error(`Invalid arguments passed to function colorPalette(): palette is not an object. (${palette})`);
     let leastDiff = 255 * 255 * 3;
     for (let i = 0; i < palette.length; i++) {
         
         const paletteColor = palette[i];
-        // if (!isColor(paletteColor)) console.error(`Invalid arguments passed to function colorPalette(): palette[${i}] is not a color. (${paletteColor})`);
+        if (!isColor(paletteColor)) console.error(`Invalid arguments passed to function colorPalette(): palette[${i}] is not a color. (${paletteColor})`);
         const diff = 
         ( originalColor[0] - paletteColor[0] ) ^ 2 +
         ( originalColor[1] - paletteColor[1] ) ^ 2 +
@@ -19,13 +19,13 @@ export function colorPalette(originalColor, palette) {
             leastDiff = diff;
         }
     }
-    if (typeof palette != "number") console.error(`Unexpected error at function colorPalette(): return value is not a number. (${i})`);
+    if (typeof result != "number") console.error(`Unexpected error at function colorPalette(): return value is not a number. (${result})`);
     return result
 }
 
-// function isColor(color) {
-//     return typeof color == "object" &&
-//     (typeof color[0] == "number") && (0 <= typeof color[0]) && (typeof color[0] <= 255) &&
-//     (typeof color[1] == "number") && (0 <= typeof color[1]) && (typeof color[1] <= 255) &&
-//     (typeof color[2] == "number") && (0 <= typeof color[2]) && (typeof color[2] <= 255)
-// }
+function isColor(color) {
+    return typeof color == "object" &&
+    (typeof color[0] == "number") && (0 <= color[0]) && (color[0] <= 255) &&
+    (typeof color[1] == "number") && (0 <= color[1]) && (color[1] <= 255) &&
+    (typeof color[2] == "number") && (0 <= color[2]) && (color[2] <= 255)
+}
