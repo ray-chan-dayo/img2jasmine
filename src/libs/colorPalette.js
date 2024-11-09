2418f094cb4acdd2c798796b0bc1ab79edf96ff7
ray-chan-dayo <64572413+ray-chan-dayo@users.noreply.github.com>
2024-11-10 02:04:22 +0900

    commented out errorhandling

--- src/libs/colorPalette.js
+++ src/libs/colorPalette.js
@@ -2,14 +2,14 @@
 export function colorPalette(originalColor, palette) {
     let result;
     // originalColor = [255,255,255]; //[r,g,b]
-    if (!isColor(originalColor)) console.error(`Invalid arguments passed to function colorPalette(): originalColor is not a color. (${originalColor})`);
+    // if (!isColor(originalColor)) console.error(`Invalid arguments passed to function colorPalette(): originalColor is not a color. (${originalColor})`);
     // エラー処理
-    if (typeof palette != "object") console.error(`Invalid arguments passed to function colorPalette(): palette is not an object. (${palette})`);
+    // if (typeof palette != "object") console.error(`Invalid arguments passed to function colorPalette(): palette is not an object. (${palette})`);
     let leastDiff = 255 * 255 * 3;
     for (let i = 0; i < palette.length; i++) {
         
         const paletteColor = palette[i];
-        if (!isColor(paletteColor)) console.error(`Invalid arguments passed to function colorPalette(): palette[${i}] is not a color. (${paletteColor})`);
+        // if (!isColor(paletteColor)) console.error(`Invalid arguments passed to function colorPalette(): palette[${i}] is not a color. (${paletteColor})`);
         const diff = 
         ( originalColor[0] - paletteColor[0] ) ^ 2 +
         ( originalColor[1] - paletteColor[1] ) ^ 2 +
@@ -23,9 +23,9 @@ export function colorPalette(originalColor, palette) {
     return result
 }
 
-function isColor(color) {
-    return typeof color == "object" &&
-    (typeof color[0] == "number") && (0 <= typeof color[0]) && (typeof color[0] <= 255) &&
-    (typeof color[1] == "number") && (0 <= typeof color[1]) && (typeof color[1] <= 255) &&
-    (typeof color[2] == "number") && (0 <= typeof color[2]) && (typeof color[2] <= 255)
-}
+// function isColor(color) {
+//     return typeof color == "object" &&
+//     (typeof color[0] == "number") && (0 <= typeof color[0]) && (typeof color[0] <= 255) &&
+//     (typeof color[1] == "number") && (0 <= typeof color[1]) && (typeof color[1] <= 255) &&
+//     (typeof color[2] == "number") && (0 <= typeof color[2]) && (typeof color[2] <= 255)
+// }
