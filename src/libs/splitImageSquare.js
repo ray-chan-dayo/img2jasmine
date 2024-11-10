45b2937744063d03d13ecdc10b01ecec17c4d896
ray-chan-dayo <64572413+ray-chan-dayo@users.noreply.github.com>
2024-11-10 11:32:42 +0900

    Update splitImageSquare.js - bugfix

--- src/libs/splitImageSquare.js
+++ src/libs/splitImageSquare.js
@@ -9,15 +9,15 @@ export function splitImageSquare(colorMap, width, height, size) {
     
     const horizontalTiles = Math.ceil(width / size);
     const verticalTiles = Math.ceil(height / size);
-    const rawX = i % width
-    const rawY = Math.floor(i/width)
-    const blockX = Math.floor(rawX/size)
-    const blockY = Math.floor(rawY/size)
 
     const result = JSON.parse(JSON.stringify( // ディープコピー
         new Array(horizontalTiles).fill(new Array(verticalTiles).fill([]))
     ));
     for (let i = 0; i < width*verticalTiles*size; i++) {//縦軸のあまりを考慮
+        const rawX = i % width
+        const rawY = Math.floor(i/width)
+        const blockX = Math.floor(rawX/size)
+        const blockY = Math.floor(rawY/size)
         if(rawY >= height){
             // 下空白
             result[blockX][blockY].push(-1);
